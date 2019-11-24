26f9ab97bcabbe0aa1586dff45fa3ee3e662f1e0
Anastasiu Catalin <catalinanastas@gmail.com>
2019-11-24 13:07:01 +0200

    Added base project using Identity framework

---
+++ WikipediaUniDAW/WikipediaUniDAW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WikipediaUniDAW.Startup))]
+namespace WikipediaUniDAW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
